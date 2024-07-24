a65076b5dfd32ca218ae702055ccf70b4a99a16c
qwseYT <151243320+qwseYT@users.noreply.github.com>
2024-07-24 04:02:36 +0300

    Add files via upload

---
+++ s.py
@@ -0,0 +1,37 @@
+import discord
+import asyncio
+from discord.ext import commands
+
+# Токен вашего бота
+TOKEN = 'token'
+# ID вашего канала
+CHANNEL_ID = id
+# Основное сообщение без упоминания пользователя
+MESSAGE_TEMPLATE = "MESSAGE"
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+    
+    channel = bot.get_channel(CHANNEL_ID)
+    if channel:
+        members = channel.members
+        for member in members:
+            try:
+                message = MESSAGE_TEMPLATE.format(user_mention=member.mention)
+                await member.send(message)
+                print(f'Sent message to {member.name}')
+            except Exception as e:
+                print(f'Could not send message to {member.name}: {e}')
+            await asyncio.sleep(2)
+    else:
+        print(f'Channel with ID {CHANNEL_ID} not found')
+
+    await bot.close()
+
+bot.run(TOKEN)
